1d7a27dd58b27336063aa62370358a83f254b3a4
Revan Kurniawan <rvnkrwn@hotmail.com>
2025-02-21 07:30:34 +0700

    update and chore

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@
 
 import HeroSection from '../app/_components/HeroSection'
 import JobsRecommended from "@/app/_components/JobsRecommended";
+import VideoPlayerSection from "@/app/_components/VideoPlayerSection";
+import FituresAndServicesSection from "@/app/_components/FituresAndServicesSection";
 
 export default function Home() {
     return (
         <div className="bg-gray-50">
             <HeroSection/>
             <JobsRecommended/>
+            <VideoPlayerSection />
+            <FituresAndServicesSection />
         </div>
     );
 }
