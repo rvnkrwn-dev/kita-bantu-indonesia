14462e77567eb8c96fee9fb6142c60856799123c
arkan <darkannur@gmail.com>
2025-02-22 00:20:38 +0700

    add job detail

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -1,8 +1,9 @@
+import { MapPin, Banknote, CalendarDays, Bookmark } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
+
 type Job = {
   id: number;
-  slug: string;
   title: string;
   company: string;
   location: string;
@@ -19,7 +20,6 @@ const Card = ({ job }: { job: Job }) => {
   const {
     id,
     title,
-    slug,
     company,
     location,
     posted_at,
@@ -30,7 +30,7 @@ const Card = ({ job }: { job: Job }) => {
 
   return (
     <Link
-      href={`/jobs/${slug}?id=${id}`}
+      href={`/jobs/${id}`}
       className="rounded-md shadow-md hover:ring-2 hover:shadow-lg transition-all hover:ring-primary border border-slate-200 flex overflow-hidden min-w-[20rem]"
     >
       <div className="w-full">
@@ -51,50 +51,22 @@ const Card = ({ job }: { job: Job }) => {
         </div>
         <div className="text-sm text-slate-700 p-2">
           <div className="flex gap-1 items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="size-5 text-red-500"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-              />
-            </svg>
-
+            <MapPin size="16" className="text-red-700" />
             <p>{location}</p>
           </div>
-          <p>{salary_range}</p>
+          <div className="flex gap-1 items-center">
+            <Banknote size="15" className="text-green-700" />
+            <p>{salary_range}</p>
+          </div>
         </div>
         <div className="flex justify-between text-sm items-center text-slate-700 p-2">
           <div className="flex gap-1 items-center">
+            <CalendarDays size="15" className="text-blue-950" />
             <p>{posted_at.displayTime}</p>
             <p className="text-xs text-slate-800">. {status}</p>
           </div>
           <p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
-              />
-            </svg>
+            <Bookmark size="16" className="text-black" />
           </p>
         </div>
       </div>
--- app/jobs/[jobID]/page.tsx
+++ app/jobs/[jobID]/page.tsx
@@ -1,11 +1,133 @@
-import React from 'react'
+import {
+  MapPin,
+  Banknote,
+  CalendarDays,
+  Bookmark,
+  Building2,
+  Watch,
+  GraduationCap,
+  BriefcaseBusiness,
+} from 'lucide-react';
+import Image from 'next/image';
+import React from 'react';
 
-type Props = {}
+export async function generateStaticParams() {
+  const KBI_URL = process.env.KBI_API;
+  const res = await fetch(`${KBI_URL}`);
+  const items = await res.json();
 
-const page = (props: Props) => {
-  return (
-    <div>page</div>
-  )
+  return items.map((item: any) => ({
+    jobID: item.id.toString(),
+  }));
 }
 
-export default page
+const page = async ({ params }: { params: { jobID: string } }) => {
+  const KBI_URL = process.env.KBI_API;
+  const { jobID } = await params;
+  const res = await fetch(`${KBI_URL}/${jobID}`, {
+    next: { revalidate: 60 },
+  });
+
+  if (!res.ok) {
+    return <p>Data tidak ditemukan</p>;
+  }
+
+  const data = await res.json();
+  return (
+    <div className="flex px-12 py-20">
+      <div className="w-2/3 space-y-5">
+        <div className="p-5 shadow-sm border border-slate-300 rounded-md flex gap-5">
+          <div className="h-20 w-20 flex">
+            <Image
+              src="/images/KBI.jpeg"
+              alt={data.company}
+              width={350}
+              height={350}
+              className="object-cover grow"
+            />
+          </div>
+          <div className="space-y-5">
+            <div className="">
+              <h1 className="text-2xl text-black font-semibold">
+                {data.title}
+              </h1>
+              <div className="flex gap-1 items-center">
+                <Building2 size="15" className="text-slate-600" />
+                <p className="text-sm text-slate-600">{data.company}</p>
+              </div>
+            </div>
+            <div className="text-sm text-slate-800">
+              <div className="flex gap-1 items-center">
+                <Banknote size="15" className="text-green-700" />
+                <p>{data.salary_range}</p>
+              </div>
+              <div className="flex gap-1 items-center">
+                <Watch size="15" />
+                <p>{data.employment_type}</p>
+              </div>
+              <div className="flex gap-1 items-center">
+                <GraduationCap size="15" />
+                <p>{data.education_level}</p>
+              </div>
+              <div className="flex gap-1 items-center">
+                <BriefcaseBusiness size="15" className="text-green-950" />
+                <p>{data.experience} pengalaman</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-5 shadow-sm border border-slate-300 rounded-md space-y-3">
+          <div>
+            <h2 className="text-xl text-blue-950 font-semibold">
+              Job Description
+            </h2>
+            <div className="flex items-center gap-2">
+              <div className="h-2 w-2 rounded-full bg-blue-500 ring-2 ring-blue-300"></div>
+              <p className="text-sm">{data.description}</p>
+            </div>
+          </div>
+          <div>
+            <h2 className="text-xl text-blue-950 font-semibold">
+              Job Requirements
+            </h2>
+            <ul className="text-sm">
+              {data.requirements.map((req: any) => (
+                <li key={req} className="flex items-center gap-2">
+                  <div className="h-2 w-2 rounded-full bg-blue-500 ring-2 ring-blue-300"></div>
+                  <p>{req}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-xl text-blue-950 font-semibold">Benefits</h2>
+            <p className="text-sm">{data.benefits.join(', ')}</p>
+          </div>
+          <div className="flex  items-center justify-between border-t border-slate-300 pt-2">
+            <div className="text-sm font-medium">
+              <p>Share this job</p>
+            </div>
+            <div className="flex gap-2">
+              <button
+                type="button"
+                className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-blue-600 text-blue-600 hover:border-blue-500 hover:text-blue-500 focus:outline-none focus:border-blue-500 focus:text-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:border-blue-500 dark:text-blue-500 dark:hover:text-blue-400 dark:hover:border-blue-400"
+              >
+                Simpan Pekerjaan
+              </button>
+              <button
+                type="button"
+                className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+              >
+                Lamar Pekerjaan
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-1/3"></div>
+    </div>
+  );
+};
+
+export default page;
