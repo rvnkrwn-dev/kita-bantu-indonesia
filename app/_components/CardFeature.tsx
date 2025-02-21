4d27d90c4d376778d43f3cfdc1a579bc2394574d
Revan Kurniawan <rvnkrwn@hotmail.com>
2025-02-21 09:23:42 +0700

    update and chore

--- app/_components/CardFeature.tsx
+++ app/_components/CardFeature.tsx
@@ -1,6 +1,6 @@
 const CardFeature = ({ children } : {children: any}) => {
     return (
-        <div className="flex flex-col gap-2 justify-center items-center bg-white border border-gray-200 shadow-sm rounded-xl p-4 md:p-5">
+        <div className="flex flex-col gap-2 justify-center items-center text-center bg-white border border-gray-200 shadow-sm rounded-xl p-4 md:p-5">
             {children}
         </div>
     );
